a62546d8ed1f43d98c00da4b4a25dfc26eda4d8b
yishai <yishai5555@gmail.com>
2026-03-17 16:06:46 +0200

    01-start

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import CourseGoals from './components/CourseGoals';
+export default function App() {
+  return (
+    <div>
+      <CourseGoals />
+    </div>
+  );
+}
---
+++ src/components/CourseGoals.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+export default function CourseGoals() {
+  return (
+    <article>
+      <div>
+        <h2>TITLE</h2>
+        <p>DESCRIPTION</p>
+      </div>
+      <button>DELETE</button>
+    </article>
+  );
+}
