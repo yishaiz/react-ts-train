bb33d5ab6ec67d2a29c2fe366499c3de5973a034
yishai <yishai5555@gmail.com>
2026-03-19 16:11:07 +0200

    05

--- src/components/CourseGoals.tsx
+++ src/components/CourseGoals.tsx
@@ -1,15 +1,29 @@
-// import React from 'react'
+import { type PropsWithChildren } from 'react';
 
-// import { type PropsWithChildren, type ReactNode } from 'react';
-import {type FC, type PropsWithChildren } from 'react';
+type CourseGoalsProps = PropsWithChildren<{ title: string }>;
 
+export default function CourseGoals({ title, children }: CourseGoalsProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>DELETE</button>
+    </article>
+  );
+}
+
+
+// import {type FC, type PropsWithChildren } from 'react';
 
 // interface CourseGoalsProps {
 //   title: string;
 //   children: ReactNode;
 // }
 
-type CourseGoalsProps = PropsWithChildren<{ title: string }>;
+
+/*
 
 const CourseGoals: FC<CourseGoalsProps> = ({ title, children }) => {
   return (
@@ -23,17 +37,4 @@ const CourseGoals: FC<CourseGoalsProps> = ({ title, children }) => {
   );
 }
 export default CourseGoals
-
-/*
-export default function CourseGoals({ title, children }: CourseGoalsProps) {
-  return (
-    <article>
-      <div>
-        <h2>{title}</h2>
-        {children}
-      </div>
-      <button>DELETE</button>
-    </article>
-  );
-}
 */
